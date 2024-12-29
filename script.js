099c1d878f47029d44c41faf77b9c7aa24c4bfb6
Ethannoma27 <Ethannoma27@hotmail.com>
2024-12-29 23:46:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Initialize AOS library for scroll animations
+AOS.init({
+    duration: 1200,  // Animation duration
+    easing: 'ease-in-out', // Easing function
+    once: true  // Animation happens only once when scrolled into view
+});
+
+//
